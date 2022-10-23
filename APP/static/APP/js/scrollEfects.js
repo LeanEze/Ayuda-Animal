a27ab56596cbeb9af055b7bab19ffa04097775a7
rodrigoMRAM <rodrigomacielth@gmail.com>
2022-10-23 13:50:07 -0300

    Efectos de scroll

---
+++ APP/static/APP/js/scrollEfects.js
@@ -0,0 +1,23 @@
+
+    // init controller
+    var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+                    triggerElement: "#row",
+                    offset: 50,											
+                    triggerHook: 0.9,
+                })
+                .setClassToggle("#row", "visible")
+                .addTo(controller);
+
+
+
+new ScrollMagic.Scene({
+                    triggerElement: "#gatillo",
+                    offset: 50,											
+                    triggerHook: 0.9,
+                })
+                .setClassToggle("#gatillo", "visible")
+                .addTo(controller);
+
+
