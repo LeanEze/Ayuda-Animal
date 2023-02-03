a02b7675ffad35a606d50ad761958487ad833095
rodrigoMRAM <rodrigomacielth@gmail.com>
2023-02-03 18:43:37 -0300

    correccion de errores

--- MTV/settings.py
+++ MTV/settings.py
@@ -86,15 +86,10 @@ WSGI_APPLICATION = 'MTV.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'AyudaAnimalDB',
-        'USER':'postgres',
-        'PASSWORD':'leopaternal93',
-        'HOST': 'localhost',
-        'PORT' : '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
