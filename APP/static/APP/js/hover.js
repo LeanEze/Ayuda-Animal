38582843a32833560567ab000d61ffed7b03bbf8
rodrigoMRAM <rodrigomacielth@gmail.com>
2023-03-05 18:09:26 -0300

    Cambios de tipografia

--- APP/static/APP/js/hover.js
+++ APP/static/APP/js/hover.js
@@ -1,39 +1,39 @@
-const perro4 = document.querySelector('.perro4');
-const perro5 = document.querySelector('.perro5');
-const perro6 = document.querySelector('.perro6');
-const perro3 = document.querySelector('.perro3');
-const perro2 = document.querySelector('.perro2');
-const perro = document.querySelector('.perro');
-const vacio = document.querySelector('.vacio');
+// const perro4 = document.querySelector('.perro4');
+// const perro5 = document.querySelector('.perro5');
+// const perro6 = document.querySelector('.perro6');
+// const perro3 = document.querySelector('.perro3');
+// const perro2 = document.querySelector('.perro2');
+// const perro = document.querySelector('.perro');
+// const vacio = document.querySelector('.vacio');
 
-//ESTA ES LA FUNCION DE CLICK QUE CAMBIA LAS IMAGENES DE CAROUSEL EN LA PANTALLA PRINCIPAL
-
-
-
-
-perro.addEventListener('click',()=>{
-    vacio.innerHTML=`<img src="/static/APP/css/pedrito.jpg" alt="" class="d-block w-100 imagenes">`;
-
-})
+// //ESTA ES LA FUNCION DE CLICK QUE CAMBIA LAS IMAGENES DE CAROUSEL EN LA PANTALLA PRINCIPAL
 
 
 
-perro2.addEventListener('click',()=>{
-    vacio.innerHTML=`<img  src="/static/APP/css/brauni.jpg" alt="" class="d-block w-100 imagenes">`;
 
-})
+// perro.addEventListener('click',()=>{
+//     vacio.innerHTML=`<img src="/static/APP/css/pedrito.jpg" alt="" class="d-block w-100 imagenes">`;
 
-perro4.addEventListener('click',()=>{
-    vacio.innerHTML=`<img  src="/static/APP/css/clarens.jpg" alt="" class="d-block w-100 imagenes">`;
+// })
 
-})
 
-perro5.addEventListener('click',()=>{
-    vacio.innerHTML=`<img  src="/static/APP/css/ayudaanimalparque.jpg" alt="" class="d-block w-100 imagenes">`;
 
-})
+// perro2.addEventListener('click',()=>{
+//     vacio.innerHTML=`<img  src="/static/APP/css/brauni.jpg" alt="" class="d-block w-100 imagenes">`;
 
-perro6.addEventListener('click',()=>{
-    vacio.innerHTML=`<img  src="/static/APP/css/rubioYroma.jpg" alt="" class="d-block w-100 imagenes">`;
+// })
 
-})
+// perro4.addEventListener('click',()=>{
+//     vacio.innerHTML=`<img  src="/static/APP/css/clarens.jpg" alt="" class="d-block w-100 imagenes">`;
+
+// })
+
+// perro5.addEventListener('click',()=>{
+//     vacio.innerHTML=`<img  src="/static/APP/css/ayudaanimalparque.jpg" alt="" class="d-block w-100 imagenes">`;
+
+// })
+
+// perro6.addEventListener('click',()=>{
+//     vacio.innerHTML=`<img  src="/static/APP/css/rubioYroma.jpg" alt="" class="d-block w-100 imagenes">`;
+
+// })
