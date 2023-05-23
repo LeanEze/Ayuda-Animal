621101233972e17dc0c4647bb5637de764182430
rodrigoMRAM <rodrigomacielth@gmail.com>
2023-05-23 17:38:56 -0300

    Error 404 template

--- MTV/settings.py
+++ MTV/settings.py
@@ -87,14 +87,21 @@ WSGI_APPLICATION = 'MTV.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ayuda',
+#         'USER': 'postgres',
+#         'PASSWORD': 'thiago123',
+#         'HOST': 'localhost', 
+#         'DATABASE_PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ayuda',
-        'USER': 'postgres',
-        'PASSWORD': 'thiago123',
-        'HOST': 'localhost', 
-        'DATABASE_PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
